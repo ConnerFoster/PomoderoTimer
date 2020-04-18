97dbc6b31a68a48d7d84263d51aab1186a6f302d
Conner Foster <connerf99@gmail.com>
2020-04-18 00:39:26 -0400

    timer logic mostly done

--- main.js
+++ main.js
@@ -2,6 +2,31 @@ const timerText = document.getElementById('timerText');
 const pomoButton = document.getElementById('pomodero');
 const shortBreakButton = document.getElementById('short-break');
 
-function timer(minutes) {
-    
+let countdown;
+
+function timer(seconds) {
+    const currentTime = Date.now();
+    const userTime = currentTime + seconds*1000;
+    displayTime(seconds);
+
+    countdown = setInterval(() => {
+        const secondsRemaining = Math.round((userTime - Date.now()) / 1000);
+
+        if(secondsRemaining < 0) {
+            clearInterval(countdown)
+            return;
+        }
+        displayTime(secondsRemaining);
+    }, 1000);
 }
+
+function displayTime(seconds) {
+   const minutes = Math.floor(seconds / 60);
+   const remainderSeconds = seconds % 60;
+   let display = minutes + ':' + remainderSeconds;
+   timerText.textContent = display;
+   while(remainderSeconds < 10) {
+       display = minutes + ':0' + remainderSeconds;
+   }
+}
+
