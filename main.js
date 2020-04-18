5c4aa308505cad2c0b2ee9254e4c7911c657fad2
Conner Foster <connerf99@gmail.com>
2020-04-17 23:20:52 -0400

    Saving to work on pc

---
+++ main.js
@@ -0,0 +1,7 @@
+const timerText = document.getElementById('timerText');
+const pomoButton = document.getElementById('pomodero');
+const shortBreakButton = document.getElementById('short-break');
+
+function timer(minutes) {
+    
+}
